527cb1b37f982448f1c97c1c45347ca0c01b5b48
Rasadus03 <rasadus@gmail.com>
2025-04-02 15:11:25 +0200

    Fixes

--- cymbal-eats-fe/src/components/OrderDetails.js
+++ cymbal-eats-fe/src/components/OrderDetails.js
@@ -9,7 +9,7 @@ function OrderDetails({  restaurants, customer, orders}) {
   const order = orders.find((element) => {
     return element.orderId == parseInt(id);
   });
-  const [orderDetails, setOrderDetails] = useState({});
+  const [orderDetails, setOrderDetails] = useState();
   const [data, setData] = useState({});
 
     useEffect(() => {
