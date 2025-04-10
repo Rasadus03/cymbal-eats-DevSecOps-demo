1a86a8983ce36e75ccaa3ed59a0c726efd572128
screendemo <demo@raniamoh.demo.altostrat.com>
2025-04-10 11:26:40 -0700

    reverting back to starting point - RA

--- cymbal-eats-fe/src/components/HomePage.js
+++ cymbal-eats-fe/src/components/HomePage.js
@@ -4,18 +4,12 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function HomePage({customer}) {
-  // Check if the customer object exists and has a name
-  const userName = customer && customer.get("name") ? customer.get("name") : null;
 
   return (
     <div className="home-page">
-      {userName ? (
-        <h1>Welcome, {userName}!</h1>
-      ) : (
-        <h1>Welcome to Cymbal Eats Food Delivery!</h1>
-      )}
+    <h1>Welcome to Cymbal Eats Food Delivery!</h1>
       <p>Explore our delicious options from various restaurants.</p>
-      <Link to="/restaurants" className='link-to-restaurants-button'>Browse Restaurants</Link>
+        <Link to="/restaurants" className='link-to-restaurants-button'>Browse Restaurants</Link>
     </div>
   );
 }
