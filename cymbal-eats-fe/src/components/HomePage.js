dc337f6ef247049645ab49607e12456f76c409ce
screendemo <demo@raniamoh.demo.altostrat.com>
2025-04-10 13:33:47 -0700

    feature: update personalization

--- cymbal-eats-fe/src/components/HomePage.js
+++ cymbal-eats-fe/src/components/HomePage.js
@@ -7,7 +7,11 @@ function HomePage({customer}) {
  
   return (
     <div className="home-page">
-    <h1>Welcome to Cymbal Eats Food Delivery!</h1>
+      {customer instanceof Map ? (
+        <h1>Welcome, {customer.get("name")} to Cymbal Eats Food Delivery!</h1>
+      ) : (
+        <h1>Welcome to Cymbal Eats Food Delivery!</h1>
+      )}
       <p>Explore our delicious options from various restaurants.</p>
         <Link to="/restaurants" className='link-to-restaurants-button'>Browse Restaurants</Link>
     </div>
@@ -15,11 +19,15 @@ function HomePage({customer}) {
 }
 
 HomePage.propTypes = {
-  customer: PropTypes.shape({
+  customer: PropTypes.oneOfType([
+    PropTypes.instanceOf(Map),
+    PropTypes.shape({
       name: PropTypes.string.isRequired,
       email: PropTypes.string.isRequired,
       uuid: PropTypes.string.isRequired,
       photoURL: PropTypes.string.isRequired,
-    }).isRequired,};
+    }),
+  ]).isRequired,
+};
 
 export default HomePage;
