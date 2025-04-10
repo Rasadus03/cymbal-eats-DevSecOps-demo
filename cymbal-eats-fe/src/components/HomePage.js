ec3e695c32c9ee58389f9ad1a387bfa2be874dea
screendemo <demo@raniamoh.demo.altostrat.com>
2025-04-10 09:26:59 -0700

    feature: Add personalization

--- cymbal-eats-fe/src/components/HomePage.js
+++ cymbal-eats-fe/src/components/HomePage.js
@@ -4,15 +4,22 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function HomePage({customer}) {
- 
+  // Check if the customer object exists and has a name
+  const userName = customer && customer.get("name") ? customer.get("name") : null;
+
   return (
     <div className="home-page">
-    <h1>Welcome to Cymbal Eats Food Delivery!</h1>
+      {userName ? (
+        <h1>Welcome, {userName}!</h1>
+      ) : (
+        <h1>Welcome to Cymbal Eats Food Delivery!</h1>
+      )}
       <p>Explore our delicious options from various restaurants.</p>
-        <Link to="/restaurants" className='link-to-restaurants-button'>Browse Restaurants</Link>
+      <Link to="/restaurants" className='link-to-restaurants-button'>Browse Restaurants</Link>
     </div>
   );
 }
+
 HomePage.propTypes = {
   customer: PropTypes.shape({
       name: PropTypes.string.isRequired,
